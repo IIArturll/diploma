6b936bb83643cdbd8287bda7589bfcccad0f9fab
Артур <sharam.artur@gmail.com>
2025-03-13 05:17:05 +0300

    Update UnauthorizedAccessException.java

--- backend/common-classes/src/main/java/artur/proj/commonclasses/exceptions/boards/UnauthorizedAccessException.java
+++ backend/common-classes/src/main/java/artur/proj/commonclasses/exceptions/boards/UnauthorizedAccessException.java
@@ -3,7 +3,7 @@ package artur.proj.commonclasses.exceptions.boards;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND) // 404 Not Found
+@ResponseStatus(HttpStatus.FORBIDDEN) // 403 Forbidden
 public class UnauthorizedAccessException extends RuntimeException {
     public UnauthorizedAccessException() {
         super("User does not have access to this board");
